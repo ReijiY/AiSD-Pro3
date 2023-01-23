d686f1f39e78a6cd6399eab70971995209e17386
Dariusz Strojny <dstrojny99@gmail.com>
2023-01-23 13:24:11 +0100

    Schematy blokowe, wstęp do sprawozdania

--- main.cpp
+++ main.cpp
@@ -2,15 +2,79 @@
 #include <vector>
 #include <math.h>
 
-#include "projekt.h"
-
-// struktura grafu przechowująca dwuwymiarową tablicę zawierającą informacje o grafie oraz ilość jego wierzchołków
+// struktura grafu przechowująca macierz sąsiedztwa oraz ilość wierzchołków grafu
 struct {
     // tablica dwuwymiarowa przechowująca informacje o krawędziach grafu
     int** array;
-    // wymiary grafu
+    // ilość wierzchołków grafu
     int size;
 } typedef graph;
+
+graph new_graph(int size);
+void free_graph(graph& g);
+void graph_add_edge(graph& g, int a, int b);
+void graph_add_edge(graph& g, const std::vector<std::pair<int, int>>& edges);
+void print_graph(const graph& g);
+void graph_print_neighbours_of_each_vertex(const graph& g);
+void graph_print_each_vertex_that_is_neighbour(const graph& g);
+void graph_print_vertex_outdegrees(const graph& g);
+void graph_print_vertex_indegrees(const graph& g);
+void graph_print_isolated_vertexes(const graph& g);
+void graph_print_loops(const graph& g);
+void graph_print_multi_edges(const graph& g);
+
+int main()
+{
+    // utworzenie grafu zawierającego 6 wierzchołków
+    graph g = new_graph(6);
+
+    // wpisanie krawędzi do grafu
+    graph_add_edge(g, {
+        {0, 1},
+        {1, 2},
+        {2, 2},
+        {1, 3},
+        {3, 1},
+        {2, 4},
+        {4, 0},
+        {4, 3}
+        });
+
+    // wypisanie macierzy sąsiedztwa znajdującej się w grafie
+    print_graph(g);
+
+    // 1) wypisanie wszystkich sąsiadów dla każdego wierzchołka grafu.
+    std::cout << "Sasiedzi dla kazdego wierzcholka grafu: \n";
+    graph_print_neighbours_of_each_vertex(g);
+
+    // 2) wypisanie wszyskich wierzchołków, które są sąsiadami każdego wierzchołka
+    std::cout << "Wierzcholki, ktore sa sasiadami kazdego wierzcholka: \n";
+    graph_print_each_vertex_that_is_neighbour(g);
+
+    // 3) wypisanie stopni wychodzących wszystkich wierzchołków
+    std::cout << "Stopnie wychodzace: \n";
+    graph_print_vertex_outdegrees(g);
+
+    // 4) wypisanie stopni wchodzących wszystkich wierzchołków
+    std::cout << "Stopnie wchodzace:\n";
+    graph_print_vertex_indegrees(g);
+
+    // wypisanie wszystkich wierzchołków izolowanych
+    std::cout << "Wierzcholki izolowane: \n";
+    graph_print_isolated_vertexes(g);
+
+    // wypisanie wysztkich pętli
+    std::cout << "Petle: \n";
+    graph_print_loops(g);
+
+    // wypisanie wszystkich krawędzi dwukierunkowych
+    std::cout << "Krawedzie dwukierunkowe: \n";
+    graph_print_multi_edges(g);
+
+    // zwolnienie pamięci struktury grafu
+    free_graph(g);
+    return 0;
+}
 
 // inicjalizacja struktury grafu tablicą wypełnioną zerami
 graph new_graph(int size)
@@ -22,7 +86,7 @@ graph new_graph(int size)
     g.size = size;
 
     // zainicjowanie tablicy wskaźników
-    g.array = new int*[size];
+    g.array = new int* [size];
 
     int i, j;
     // dla każdego elementu tablicy
@@ -38,7 +102,7 @@ graph new_graph(int size)
 }
 
 // zwolnienie pamięci zajętej przez strukturę grafu
-void free_graph(graph &g)
+void free_graph(graph& g)
 {
     // dla każdego elementu w tablicy
     for (int i = 0; i < g.size; i++)
@@ -48,10 +112,30 @@ void free_graph(graph &g)
     }
     // zwolnienie pamięci tablicy
     delete[] g.array;
-    
+
     // wyzerowanie elementów w strukturze
     g.array = NULL;
     g.size = 0;
+}
+
+// dodanie krawędzi między wierzchołkami a i b do grafu
+void graph_add_edge(graph& g, int a, int b)
+{
+    // jeżeli podane wierzchołki znajduja się w zakresie grafu
+    if (a < 0 || a >= g.size || b < 0 || b >= g.size) throw "Podane wierzcholki nie znajduja sie w grafie!";
+    // wpisanie istnienia krawędzi do macierzy sąsiedztwa
+    g.array[a][b] = 1;
+}
+
+// przeciążenie funkcji graph_add_edge przyjmujące tablicę par numerów wierzchołków tworzących krawędzie
+void graph_add_edge(graph& g, const std::vector<std::pair<int, int>>& edges)
+{
+    // dla każdej pary wierzchołków
+    for (const std::pair<int, int>& edge : edges)
+    {
+        // utworzenie krawędzi między nimi
+        graph_add_edge(g, edge.first, edge.second);
+    }
 }
 
 // wypisanie zawartości tablicy relacji między wierzchołkami grafu
@@ -85,12 +169,12 @@ void graph_print_neighbours_of_each_vertex(const graph& g)
     for (i = 0; i < g.size; i++)
     {
         print_count = 0;
-        
+
         // wypisanie numeru wierzchołka
         printf("%2d: ", i);
 
         // dla każdej kolumny w tablicy
-        for (j = 0; j < g.size; j++) 
+        for (j = 0; j < g.size; j++)
         {
             // jeżeli istnieje krawędź w grafie od wierzchołka i do j
             if (g.array[i][j] == 1)
@@ -238,77 +322,4 @@ void graph_print_multi_edges(const graph& g)
         }
     }
     printf("\n");
-}
-
-// dodanie krawędzi między wierzchołkami a i b do grafu
-void graph_add_edge(graph& g, int a, int b)
-{
-    // jeżeli podane wierzchołki znajduja się w zakresie grafu
-    if (a < 0 || a >= g.size || b < 0 || b >= g.size) throw "Podane wierzcholki nie znajduja sie w grafie!";
-    // wpisanie istnienia krawędzi do macierzy sąsiedztwa
-    g.array[a][b] = 1;
-}
-
-// przeciążenie funkcji graph_add_edge przyjmujące tablicę par numerów wierzchołków tworzących krawędzie
-void graph_add_edge(graph& g, const std::vector<std::pair<int, int>>& edges)
-{
-    // dla każdej pary wierzchołków
-    for (const std::pair<int, int>& edge : edges)
-    {
-        // utworzenie krawędzi między nimi
-        graph_add_edge(g, edge.first, edge.second);
-    }
-}
-
-int main()
-{
-    // utworzenie grafu zawierającego 6 wierzchołków
-    graph g = new_graph(6);
-
-    // wpisanie krawędzi do grafu
-    graph_add_edge(g, {
-        {0, 1},
-        {1, 2},
-        {2, 2},
-        {1, 3},
-        {3, 1},
-        {2, 4},
-        {4, 0},
-        {4, 3}
-        });
-
-    // wypisanie macierzy sąsiedztwa znajdującej się w grafie
-    print_graph(g);
-
-    // 1) wypisanie wszystkich sąsiadów dla każdego wierzchołka grafu.
-    std::cout << "Sasiedzi dla kazdego wierzcholka grafu: \n";
-    graph_print_neighbours_of_each_vertex(g);
-
-    // 2) wypisanie wszyskich wierzchołków, które są sąsiadami każdego wierzchołka
-    std::cout << "Wierzcholki, ktore sa sasiadami kazdego wierzcholka: \n";
-    graph_print_each_vertex_that_is_neighbour(g);
-
-    // 3) wypisanie stopni wychodzących wszystkich wierzchołków
-    std::cout << "Stopnie wychodzace: \n";
-    graph_print_vertex_outdegrees(g);
-
-    // 4) wypisanie stopni wchodzących wszystkich wierzchołków
-    std::cout << "Stopnie wchodzace:\n";
-    graph_print_vertex_indegrees(g);
-
-    // wypisanie wszystkich wierzchołków izolowanych
-    std::cout << "Wierzcholki izolowane: \n";
-    graph_print_isolated_vertexes(g);
-
-    // wypisanie wysztkich pętli
-    std::cout << "Petle: \n";
-    graph_print_loops(g);
-
-    // wypisanie wszystkich krawędzi dwukierunkowych
-    std::cout << "Krawedzie dwukierunkowe: \n";
-    graph_print_multi_edges(g);
-
-    // zwolnienie pamięci struktury grafu
-    free_graph(g);
-    return 0;
 }
